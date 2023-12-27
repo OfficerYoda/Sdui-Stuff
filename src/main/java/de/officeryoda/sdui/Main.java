6d196313e8b238c4d101e245ecd86b175d5ae0e7
OfficerYoda <officeryoda@gmail.com>
2023-12-27 19:49:55 +0100

    Refactoring

---
+++ src/main/java/de/officeryoda/sdui/Main.java
@@ -0,0 +1,8 @@
+package de.officeryoda.sdui;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SduiApiHandler sduiApiHandler = new SduiApiHandler();
+    }
+}
--- src/main/java/de/officeryoda/sdui/UserInformation.java
+++ src/main/java/de/officeryoda/sdui/UserInformation.java
@@ -4,7 +4,7 @@ import de.officeryoda.sdui.responseSubclasses.Data;
 import de.officeryoda.sdui.responseSubclasses.Meta;
 
 @lombok.Data
-public class SduiApiResponse {
+public class UserInformation {
 
     private Data data;
     private String status;
